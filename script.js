ade97096f42f10728cb19e3a4c6e4f0825d21d5f
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-07-04 19:27:35 +1000

    Wow

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+    fetch('http://anypost.pixel-fy.com/dog.php')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendData(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+    function appendData(data) {
+        var mainContainer = document.getElementById("image");
+
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = '<img src="' + data[i].message + '" width="100%" > ';
+            mainContainer.appendChild(div);
+        }
+    }
