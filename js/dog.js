1db9cabbec82c421b7c089e8e317a19e3d511e31
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-07-13 20:37:07 +1000

    Changed API to my own and changed Nav

--- js/dog.js
+++ js/dog.js
@@ -1,4 +1,4 @@
-fetch('http://random-dog-api.herokuapp.com/dog')
+fetch('https://random-dog-api.herokuapp.com/dog/')
     
 .then(function (response) {
     return response.json();
@@ -14,7 +14,7 @@ var mainContainer = document.getElementById("image");
 
 for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    div.innerHTML = '<img  src="' + data[i].image + '" width="100%" > ';
+    div.innerHTML = '<img  src="' + data[i].image + '" width="100%" ></div><div class="buttons"><a onClick="window.location.reload();"><buttton class="badge"><i class="fas fa-redo"></i> New Image</button></a><br><div class="fact"><h2>Random Dog Fact: </h2><p>' + data[i].fact + '</p></div>';
     mainContainer.appendChild(div);
 
 }
